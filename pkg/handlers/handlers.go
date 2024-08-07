49d2f6936a7977f054d0e9deed1b0cdc9df9c488
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-08-07 21:18:47 +0330

    Update handlers.go

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,9 +1,12 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
 	"golang-hotel-booking/pkg/config"
 	"golang-hotel-booking/pkg/models"
 	"golang-hotel-booking/pkg/render"
+	"log"
 	"net/http"
 )
 
@@ -25,7 +28,7 @@ func (m *Repository) Home(res http.ResponseWriter, req *http.Request) {
 
 	remoteIp := req.RemoteAddr
 	m.App.Sessions.Put(req.Context(), "remote_ip", remoteIp)
-	render.RenderTemplate(res, "home.page.gohtml", &models.TemplateData{})
+	render.RenderTemplate(res, "home.page.gohtml", &models.TemplateData{}, req)
 
 }
 
@@ -34,24 +37,52 @@ func (m *Repository) About(res http.ResponseWriter, req *http.Request) {
 	stringMap["test"] = "1.0.0"
 	remoteIp := m.App.Sessions.GetString(req.Context(), "remote_ip")
 	stringMap["remote_ip"] = remoteIp
-	render.RenderTemplate(res, "about.page.gohtml", &models.TemplateData{StringMap: stringMap})
+	render.RenderTemplate(res, "about.page.gohtml", &models.TemplateData{StringMap: stringMap}, req)
 
 }
 func HandlerICon(w http.ResponseWriter, r *http.Request) {}
 
 func (m *Repository) Reservation(res http.ResponseWriter, req *http.Request) {
-	render.RenderTemplate(res, "makeres.page.gohtml", &models.TemplateData{})
+	render.RenderTemplate(res, "makeres.page.gohtml", &models.TemplateData{}, req)
 }
 
 func (m *Repository) Luxury(res http.ResponseWriter, req *http.Request) {
-	render.RenderTemplate(res, "luxury.page.gohtml", &models.TemplateData{})
+	render.RenderTemplate(res, "luxury.page.gohtml", &models.TemplateData{}, req)
 }
 func (m *Repository) Roof(res http.ResponseWriter, req *http.Request) {
-	render.RenderTemplate(res, "roof.page.gohtml", &models.TemplateData{})
+	render.RenderTemplate(res, "roof.page.gohtml", &models.TemplateData{}, req)
 }
 func (m *Repository) Search(res http.ResponseWriter, req *http.Request) {
-	render.RenderTemplate(res, "search.page.gohtml", &models.TemplateData{})
+	render.RenderTemplate(res, "search.page.gohtml", &models.TemplateData{}, req)
 }
 func (m *Repository) Contact(res http.ResponseWriter, req *http.Request) {
-	render.RenderTemplate(res, "contact.page.gohtml", &models.TemplateData{})
+	render.RenderTemplate(res, "contact.page.gohtml", &models.TemplateData{}, req)
+}
+func (m *Repository) PostSearch(res http.ResponseWriter, req *http.Request) {
+	start := req.Form.Get("start_date")
+	end := req.Form.Get("ending_date")
+
+	res.Write([]byte(fmt.Sprintf("start: %s\nend: %s", start, end)))
+}
+
+type jsonResponse struct {
+	Ok      bool   `json:"ok"`
+	Message string `json:"message"`
+}
+
+func (m *Repository) SearchJson(res http.ResponseWriter, req *http.Request) {
+	resp := jsonResponse{
+		Ok:      false,
+		Message: "available!",
+	}
+	out, err := json.Marshal(resp)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(string(out))
+	res.Header().Set("Content-Type", "application/json")
+	_, err = res.Write(out)
+	if err != nil {
+		return
+	}
 }
