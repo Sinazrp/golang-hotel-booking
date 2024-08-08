affc863a2fc7ce1f319a2cb27186f70983c602e7
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-08-08 11:34:44 +0330

    Update main.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/alexedwards/scs/v2"
-	"golang-hotel-booking/pkg/config"
-	"golang-hotel-booking/pkg/handlers"
-	"golang-hotel-booking/pkg/render"
+	"golang-hotel-booking/internal/config"
+	"golang-hotel-booking/internal/handlers"
+	"golang-hotel-booking/internal/render"
 	"log"
 	"net/http"
 	"time"
